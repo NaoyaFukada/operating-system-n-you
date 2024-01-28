068677bf7a2c93f7612e39f34933edfde29a34d9
Naoya <naoyafukada@icloud.com>
2024-01-28 15:10:02 +1000

    28 Jan 3:09 pm 2024

--- bash/command/user-management.sh
+++ bash/command/user-management.sh
@@ -19,27 +19,3 @@ sudo useradd userName
 
 # Delete user account
 sudo userdel userName
-
-# Permission Management:
-    # Add the executable permission to the owner
-    chmod u+x fileName
-    # Remove the executable permission from the owner
-    chmod u-x fileName
-    # Add read or executable permission to the owner
-    chmod u+rx fileName
-    # Add read permission to the owner, group, and other users
-    chmod ugo+r fileName
-
-# Numerical Permission management:
-    # order of this 3 digit is owner, group and all other users from left to right.Example, 7 is 4+2+1 (equals read, write, execute permission)
-    chmod 754 fileName # RWX for owner, RX for group users, and 4 for all other users
-    # The code above is same with 3 individual codes below:
-    chmod u+rwx fileName
-    chmod g+rx fileName
-    chmod o+r fileName
-
-# Change owner of the file
-sudo chown userName fileName
-
-# Change group of the file
-sudo chgrp groupName fileName
