215ffe366f851ab95aed4047cc8f59bb16b9952c
Naoya <naoyafukada@icloud.com>
2024-02-16 14:26:27 +1000

    16 Feb 2:26pm 2024

---
+++ powershell/command/package.ps1
@@ -0,0 +1,6 @@
+# Run the executable file (You can do this by just specifying the path)
+C:\Users\Cindy\Desktop\hello.exe
+
+# To display help information or the list of supported sub-commands and options for a particular executable file
+installer.exe /?
+
