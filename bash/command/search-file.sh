7dafe738e48038060d49a2be7be1b1bd3d01e20c
Naoya <naoyafukada@icloud.com>
2024-02-21 14:32:05 +1000

    21 Feb 2:32pm 2024

--- bash/command/search-file.sh
+++ bash/command/search-file.sh
@@ -1,2 +1,5 @@
 # To search within files for lines that match a specified pattern, useful for locating specific text in single or multiple files:
 grep cow farm_animals.txt
+
+# Search Example
+grep -r "random" javascript
